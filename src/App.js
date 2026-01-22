65a0ac676d7d0507696cd7db03cf5a4a50d6355b
Ruslan379 <3325528@ukr.net>
2026-01-22 18:08:50 +0200

    step 23

--- src/App.js
+++ src/App.js
@@ -34,9 +34,9 @@ export default function App() {
   return (
     <div>
       <Painting
-        url={paintings[0].url}
+        // url={paintings[0].url}
         title={paintings[0].title}
-        author={paintings[0].author.tag}
+        // author={paintings[0].author.tag}
         profileUrl={paintings[0].author.url}
         price={paintings[0].price}
         quantity={paintings[0].quantity}
