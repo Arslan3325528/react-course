8242dbcb2d73dea659637ce97a40b9f674f68b37
Ruslan379 <3325528@ukr.net>
2026-01-22 07:34:30 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 // import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 // ---------------------------------------------------------
 
 
-import React from 'react';
+// import React from 'react';
 
 //! 1 - React-елементи
-const element = React.createElement("div");
+// const element = React.createElement("div");
 
 // const element = React.createElement("div", {
 //   a: 5,
@@ -29,4 +29,4 @@ const element = React.createElement("div");
 //   children: ["Привет", " ", "мир!"]
 // });
 
-console.log("element:", element);
+// console.log("element:", element);
