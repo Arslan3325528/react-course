9089336aa347b76af373fb2d917029f16736452c
Ruslan379 <3325528@ukr.net>
2026-01-22 14:47:30 +0200

    step 16

--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@
 
 import React from 'react'; //! 1 - React-елементи
 import ReactDOM from 'react-dom/client'; //! 2 - Render React-елемента
+import paintings from './paintings.json' //! 6.1 - Компонент `Painting`
+import planes from './planes.json'
 
 //! 1 - React-елементи
 // const element = React.createElement("div");
@@ -84,7 +86,7 @@ import ReactDOM from 'react-dom/client'; //! 2 - Render React-елемента
 // root.render(element);
 
 
-//! 5 - Рендер Вкладених JSX-елементів 
+//! 5 - Рендер Вкладених JSX-елементів (інтерполяція)
 // const elem1 = <span>Привет </span>;
 // const elem2 = <span>мир!</span>;
 
@@ -124,9 +126,21 @@ let painting = (
   </div>
 );
 
-const data = {
+let data = {
   "id": "id-1",
   "url": "https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg",
+  "url1": "https://lh3.googleusercontent.com/pw/AP1GczNs6n74d0t3SgOMv9k5_PsOazCTuNdWwWJw3DA34U2Of-Uua4S3EItO-W0ChLissOAyto7UkYnq20Co_6q-8gjdhL_Cyea1ErpxtOCu8DR7Ea4AFIcJot1Uz3gK2jmuNgWhcjeR0NLgpDxagwu9MmZ8=w924-h727-s-no-gm?authuser=0",
+  "url2": "https://lh3.googleusercontent.com/pw/AP1GczNGlg1WrUIrHaldyLzqhtpjcd3ivMq-MuGEBPsz_qYczTWm_l8wJ8eas5nAeRYk3zZs7EzT1tiJa0SnBfFNcM6Lk4C7P1PnyDxBxhVZ0cBBZ3EeB1_NkGx5ntFsODL3iAM6k21UtFPMsax9xWPyh3WI=w938-h950-s-no-gm?authuser=0",
+  "url3": "https://lh3.googleusercontent.com/pw/AP1GczPlOJa_nz3Ch08sMU0GWfU3ZsRJsitvgpvb1tBucxFF2I7J89X1JhWGP-7BwF_LdZHkk2kbWWx8msIXr4YId3DiOkbmnLI91WJik0QPR5VmE0QoV1sY8HrOQztzLhCxVro3bA9kO-PGzA0Vt0iOUHIZ=w848-h847-s-no-gm?authuser=0",
+  "url4": "https://lh3.googleusercontent.com/pw/AP1GczOOPA6CWmNQnGEW6RH5j_eZyyLrLDfJZKnwAdVgskpMYGAZeXCQy4baC95vob7YfZJlVWtxkKakgFcEIcdFYqMNgMtbs9vUgBN8n-KsQWnK71O837Tcg_pFkAv1sUHOzH_RFXKb0MRC6rWuAEknQ-8S=w937-h962-s-no-gm?authuser=0",
+  "url5": "https://lh3.googleusercontent.com/pw/AP1GczMKLc4BcncR6OVS1d8lmOPkBwRxdEhtDfpcEhgxey8Uq8Yjph7qANZZ3nAmMWqgKbrJQllzQ9g0GnZJAB63dc_gor8_XwNrRNdEuntxwk-B7EpmNyHoXBUjn2KY8HaSMMIMI6WqzmSetHeTSls61FUs=w908-h776-s-no-gm?authuser=0",
+  "url6": "https://lh3.googleusercontent.com/pw/AP1GczNyRJlWO2ki4tSpbCXhowMOcD6DCnUOqdDbrrMuObFy8W7mv6uKdL8K_HUO2N7OlNQaN2U5BDQTuatfMaaVjuhD9s9rJYSCq_tZQAxaQ-ydFSJ8EwF8rPsFPoZvJXyI60CKHc_YSo4y17xUPf0yiSr_=w913-h847-s-no-gm?authuser=0",
+  "url7": "https://lh3.googleusercontent.com/pw/AP1GczN2lll11uaDdq8QzGve7tLHZ1oPqLtovR7IYe6EDuyNxb8EfpHVI7TI62USXr7EF9QuBJVgMpA8OW9g5FezEasyHHk7WPaLe4phsI0A7Vsk6Fv482jFXJjhn4lp3vaa8gI_-UTnRMG7l0ZsrYctks8U=w738-h708-s-no-gm?authuser=0",
+  "url8": "https://lh3.googleusercontent.com/pw/AP1GczOMavfHaYVT4eTIeMRu7Nf0sCXN6W5n2fjjpna_xFwpNWe2F4smHwIEGN_HY2aDibjQrABBD7JhPCvrZHtfN2jOP4ilY-rD94SLpTB62YeBBOLrVzKEQ7y-ck57_vHkvteGiAG23FrQYXA5mQeriSIx=w916-h907-s-no-gm?authuser=0",
+  "url9": "https://lh3.googleusercontent.com/pw/AP1GczMW9bQPJS-5H0JL5TtRZ7ZEpl1nxf8P1-KGsHff6BkWUFMO7vxLBrhlhQdbNDZQJl7W0lrUZtUf_RSSU6bNhQPTQ7W2qntlaabchF_RK2BZKKbygFUKP8KmlP6Q2yn8RCVQhmB0emkPoilDP5sRqre7=w944-h911-s-no-gm?authuser=0",
+  "url10": "https://lh3.googleusercontent.com/pw/AP1GczNBzI4pES1NPiwOlSVtrOB8HI05jTeAmviNH4z9LxzgOMyt0x3kbW037JCesGSWonYwro6_zNTdpRSOs_qO4PGaReHNHeQvu5LdEnwCoroJbKn2DTCXR_G6tj0leAJXOK1wgyTosj4d4of3Vh2yfGPO=w938-h986-s-no-gm?authuser=0",
+  "url11": "https://lh3.googleusercontent.com/pw/AP1GczMtY4nQTvqUvzjmYtxqfDj7u1ncZYyvpoy3ES_4xaIF6tKXLK5tRMfCzm88lHcLuhmRFZaceL8y_dvI_J-3Yd4bupWnF9O23D8kAuu8TwZNJzSxouDVFLmDfu4yTld_dBasPhG0OqNrPu-jZRMwcZE8=w931-h929-s-no-gm?authuser=0",
+
   "title": "Feathers. Art abstract",
   "price": 500,
   "author": {
@@ -135,6 +149,13 @@ const data = {
   },
   "quantity": 10
 };
+
+//! 6.2 - Компонент `Painting` з json
+data = paintings[0];
+// data = paintings[1];
+// data = paintings[2];
+
+// data = planes[1];
 
 painting = (
   <div>
@@ -149,3 +170,5 @@ painting = (
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(painting);
+
+
